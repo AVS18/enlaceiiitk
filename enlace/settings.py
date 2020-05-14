a74dea19f3e7e5f84d65d6d695d9fbd90b46e15d
AVS18 <avsadityavardhan18bcs@iiitkottayam.ac.in>
2020-05-14 11:57:44 +0530

    seventh

--- enlace/settings.py
+++ enlace/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jvlhkdq*qfenvwtcywk@l)+fm#bel_02ad_&ez7a3ny#-@t0x5'
+#SECRET_KEY = 'jvlhkdq*qfenvwtcywk@l)+fm#bel_02ad_&ez7a3ny#-@t0x5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','enlaceiiitk.herokuapp.com']
 SECRET_KEY = os.environ.get('SECRET_KEY')
